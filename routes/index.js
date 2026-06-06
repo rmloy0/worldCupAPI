ace229e1dcda7dc646cd31c2beeba73a2deb63b1
rmloyo <rmloyo@gmail.com>
2026-06-05 21:41:01 -0600

    change database

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the WorldCupAPI');
+});
+
 router.use('/players', require('./players'));
 router.use('/stadiums', require('./stadiums'));
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { initDB } = require('./database');
-
+require('dotenv').config();
 
 const port = process.env.PORT || 4000;
 const app = express();
